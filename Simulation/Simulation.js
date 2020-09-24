a33092393ecc23af082f2c0afc39e1be5752fa78
Henrique Estanislau <65318224+HenriqueEstanislau@users.noreply.github.com>
2020-09-24 14:23:10 -0300

    Update Simulation.js

--- Simulation/Simulation.js
+++ Simulation/Simulation.js
@@ -13,13 +13,25 @@ var perfilModerado = [18, 28.5, 11, 15, 27.5]
 var perfilArrojado = [28, 27, 25, 15, 5]
 var perfilConservadorTitulos = ['Pós fixado', 'Inflação', 'Internacional']
 var perfilArrojadoTitulos = ['Internacional', 'Multimercado', 'Renda Variável', 'Inflação', 'Pós Fixado']
+var pConservador = "Conservador"
+var pModerado = "Moderado"
+var pArrojado = "Arrojado"
+
 //funcao verificar disparada quando o botão "simular" é clicado
 function verificar(){
     try {
         //habilita o gráfico e desabilita as perguntas (deve ser colocado no começo para ter o efeito de animação no chart.js)
         document.getElementById('chart').style.display = "inline";
-        document.getElementById('quest').style.display = "none"; 
+        document.getElementById('quest').style.display = "none";  
+        
+        //Ancora
         window.location.href = "#title"
+
+        //escreve no HTML o valor total a ser investido
+        // var valorTot = document.querySelector('div#valortotal')
+        // let tot = slider.value * 1000
+        // valorTot.innerHTML += `<p class='valorTotal'>${tot.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</p>`
+
 
         //faz uma verificação no questionário para ver se todos os campos foram preenchidos
         var rendimentos = document.getElementsByClassName("rendimentos")
@@ -28,16 +40,19 @@ function verificar(){
             grafico(perfilConservadorTitulos, perfilConservador)
             imprimir(perfilConservadorTitulos, perfilConservador, slider)
             calculoValor(perfilConservador)
+            imprimirSubtitle(pConservador)
 
         }else if (rendimentos[1].checked) {
             grafico(perfilArrojadoTitulos, perfilModerado)
             imprimir(perfilArrojadoTitulos, perfilModerado, slider)
             calculoValor(perfilModerado)
+            imprimirSubtitle(pModerado)
 
         }else if (rendimentos[2].checked) {
             grafico(perfilArrojadoTitulos, perfilArrojado)
             imprimir(perfilArrojadoTitulos, perfilArrojado, slider)
             calculoValor(perfilArrojado)
+            imprimirSubtitle(pArrojado)
 
         }else{
             alert("Escolha uma opção na pergunta 1")
@@ -69,19 +84,25 @@ function verificar(){
             for(var k in porcentagem){
                 valorPorcentagem = porcentagem[k]
                 porcentagemTela.innerHTML += `<p>${valorPorcentagem}%</p>`
-                console.log(valorPorcentagem)
+                //console.log(valorPorcentagem)
             }
 
         }
 
         //escreve no HTML os dados da categoria, porcentagem e valor
-        function imprimir(perfilName, perfilData, slider){
+        function imprimir(perfilName){
             var res = document.querySelector('div#categoria')
             res.innerHTML += "<p class='small-title'>Categoria</p>"
             for (var i in perfilName){
                 res.innerHTML += `<p>${perfilName[i]}</p>`
             }
-        }    
+        }
+        
+        //escreve no HTML os dados do perfil de investidor
+        function imprimirSubtitle(perfilInvestidor){
+            var perfilInvest = document.querySelector('div#perfilInvest')
+            perfilInvest.innerHTML += `<p>O gráfico de rentabilidade apresentado foi gerado utilizando a Carteira Recomendada da <a href="https://www.xpi.com.br/" class="XPI" target="_blank">XP Investimentos</a>, que é compativel com o perfil ${perfilInvestidor}. Para saber mais detalhes sobre os produtos que compõem esta carteira recomendamos que você acesse o site da <a href="https://www.xpi.com.br/" class="XPI" target="_blank">XP Investimentos</a>. O perfil ${perfilInvestidor} não necessariamente será o seu perfil de investidor. Para um calculo mais preciso você pode fazer o teste na nossa plataforma em "Perfil de Investidor".<br> As informações presentes neste material técnico são baseadas em simulações e os resultados reais poderão ser significativamente diferentes.</p>`
+        }
         
     } catch (error) {
         //se algum erro inesperado acontecer
@@ -100,11 +121,11 @@ function grafico(perfilName, perfilData){
                     label: 'Investimentos',
                     data: perfilData,
                     backgroundColor: [
-                        'rgba(255, 99, 132, 0.9)',
-                        'rgba(54, 162, 235, 0.9)',
-                        'rgba(255, 206, 86, 0.9)',
-                        'rgba(75, 192, 192, 0.9)',
-                        'rgba(153, 102, 255, 0.9)'
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)'
                     ],
                     borderColor: [
                         'rgba(255, 99, 132, 1)',
@@ -120,12 +141,23 @@ function grafico(perfilName, perfilData){
                     scales: {
                         yAxes: [{
                             ticks: {
-                                beginAtZero: true
+                                beginAtZero: true,
                             }
                         }]
                     },
                     animation: {
                         duration: 1250
+                    },
+                    tooltips: {
+                        callbacks: {
+                            label: function(tooltipItem, data) {
+                            var dataset = data.datasets[tooltipItem.datasetIndex];
+
+
+                            var currentValue = dataset.data[tooltipItem.index];  
+                            return " " + currentValue + "%";
+                            }
+                        }
                     }
             }
         });
