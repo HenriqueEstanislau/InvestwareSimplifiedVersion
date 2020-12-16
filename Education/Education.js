7a516cb6e04aabc31982ddae5486d0776058336f
Henrique Estanislau <henrique.estanislau2012@gmail.com>
2020-12-16 15:55:47 -0300

    Update Education.js

--- Education/Education.js
+++ Education/Education.js
@@ -1,7 +1,7 @@
 const materiasDestaque = [
     {
         id: 0,
-        titulo: 'Quais são os primeiros passos essenciais?<br>',
+        titulo: 'Quais são os primeiros passos essenciais?',
         imagem: 'Imagem1.jpg',
         pagina: '../SingIn/SingIn.html'
     },
@@ -13,35 +13,53 @@ const materiasDestaque = [
     },
     {
         id: 2,
-        titulo: 'Ações tudo o que você precisa saber antes de investir!<br>',
+        titulo: 'Ações tudo o que você precisa saber antes de investir!',
         imagem: 'Imagem3.jpg',
         pagina: '../SingIn/SingIn.html'
     },
     {
         id: 3,
-        titulo: 'É possível viver de dividendos?<br><br>',
+        titulo: 'É possível viver de dividendos?',
         imagem: 'Imagem4.jpg',
         pagina: '../SingIn/SingIn.html'
     },
     {
         id: 4,
-        titulo: '10 Dicas de como economizar para sobrar mais dinheiro!<br>',
+        titulo: '10 Dicas de como economizar para sobrar mais dinheiro!',
         imagem: 'Imagem5.jpg',
         pagina: '../SingIn/SingIn.html'
-    }
+    }    
 ]
 
 const materiasComecar = [
     {
         id: 0,
-        titulo: 'Quais são os primeiros passos essenciais?<br>',
-        imagem: 'Imagem1.jpg',
+        titulo: 'O que é a Taxa Selic e como ela impacta seus investimentos',
+        imagem: 'Imagem6.jpg',
         pagina: '../SingIn/SingIn.html'
     },
     {
         id: 1,
-        titulo: 'Descubra os erros mais comuns ao montar sua carteira de ações!',
-        imagem: 'Imagem3.jpg',
+        titulo: 'Como ser trader? Confira 5 características essenciais!',
+        imagem: 'Imagem7.jpg',
+        pagina: '../SingIn/SingIn.html'
+    },
+    {
+        id: 2,
+        titulo: 'Aprenda 4 dicas de como fazer um bom plano de investimento!',
+        imagem: 'Imagem8.jpg',
+        pagina: '../SingIn/SingIn.html'
+    },
+    {
+        id: 3,
+        titulo: 'Quais são as melhores formas de investir com pouco dinheiro?',
+        imagem: 'Imagem9.jpg',
+        pagina: '../SingIn/SingIn.html'
+    },
+    {
+        id: 4,
+        titulo: 'Quais são os mitos sobre investir na bolsa de valores?!',
+        imagem: 'Imagem10.jpg',
         pagina: '../SingIn/SingIn.html'
     }
 ]
